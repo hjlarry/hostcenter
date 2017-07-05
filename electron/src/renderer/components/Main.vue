3e0c7cac86b80082776c1a87e778cdbe01d20c6b
hejl <hejl@iMac.local>
2017-07-05 17:16:26 +0800

    add travis

--- electron/src/renderer/components/Main.vue
+++ electron/src/renderer/components/Main.vue
@@ -6,28 +6,24 @@
         <h2>How to Use</h2>
         <ul>
             <li>
-                <p>For Mac User , Please open your terminial and type this code first</p>
+                <p>For Mac/Linux User , Please open your terminial and type this code first</p>
                 <pre><code>  sudo chmod 666 /etc/hosts </code></pre>
             </li>
             <li>
-                <p>In WeChat , You can Follow this account "gitPull" ,and then you can edit the hosts in your wechat</p>
+                <p>For Windows User , Please run this soft as Administrator mode</p>
             </li>
 
         </ul>
         <p>======================================</p>
-        <h2>Internet in China</h2>
-        <p>
-            Network review is common in all countries, and it exists not only in China. The situation in the world is not conducive to the moment of China, the authorities will filter out the impact of harm to China's long-term development of information, then the significance of national security is more significant.
-        </p>
-        <p>
-            There is a large number of unrealistic, objective and even contradictory discredited authorities on the Internet, and they are generally backed by Western governments or non-governmental organizations. These media include but are not limited to some Hong Kong media, overseas websites.
-        </p>
-        <p>
-            We hope that you can encounter such remarks and opinions, do not think without thinking, the mood was once instigated and can not restrain, blindly believe that these one-sided or distort the facts of things, but to truthfully think, to Get rid of the emotional kidnapping of the grotesque thinking to understand.
-        </p>
+        <h2>Pull Request</h2>
+
+
+            <pre><code>  https://github.com/hjlarry/hostcenter </code></pre>
+
+
         <p style="text-align: center">
             <router-link to="/localhost"><a class="btn btn-primary btn-lg" role="button">Here We Go</a></router-link>
-            </p>
+        </p>
     </div>
 </template>
 
